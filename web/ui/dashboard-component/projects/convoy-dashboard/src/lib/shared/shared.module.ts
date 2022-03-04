be621803623987aad984384d5e4fcf252f481956
Pelumi Honey <pelumioni25@gmail.com>
2022-03-04 16:37:05 +0100

    created filter component

--- web/ui/dashboard-component/projects/convoy-dashboard/src/lib/shared/shared.module.ts
+++ web/ui/dashboard-component/projects/convoy-dashboard/src/lib/shared/shared.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { pipes } from './pipes';
+import { FiltersComponent } from './components/filters/filters.component';
 
 @NgModule({
-	declarations: [...pipes],
+	declarations: [...pipes, FiltersComponent],
 	imports: [CommonModule],
 	exports: [...pipes]
 })
